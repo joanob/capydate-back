2c8314e227904f4f3e0a897a0fdafdf96678cd20
Joan Oltra Blasco <joltrablasco@gmail.com>
2025-01-07 15:49:41 +0100

    create spring boot project

---
+++ src/main/java/com/capyjella/capydate/CapydateApplication.java
@@ -0,0 +1,13 @@
+package com.capyjella.capydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapydateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapydateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capyjella/capydate/CapydateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capyjella.capydate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapydateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
